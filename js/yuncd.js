6d51e261f7e2a3100dba14e1849b08a2fce7ec82
vpnn <49159051+vpnn@users.noreply.github.com>
2020-10-21 17:12:47 +0800

    Update yuncd.js

--- js/yuncd.js
+++ js/yuncd.js
@@ -1,1 +1,1 @@
-﻿document.write('<script type="text/javascript" src="Images/cloud.js"></script>');
+﻿document.write('<script type="text/javascript" src="https://vpnn.github.io/Images/cloud.js"></script>');
